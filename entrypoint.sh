19216f39c3c685b2b82a85c68fa1937400ca7f27
Mykyta Kostiuk <kostiuk.mykyta5@google.com>
2025-09-12 21:32:27 +0200

    [GHA-01] Edit logic

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,13 +6,26 @@ set -e
 # Main script logic
 echo "Starting script execution..."
 
-if echo "$*" | grep -i -q FIXED;
-then 
-    echo "FIXED found in arguments. Executing fixed task..."
-    # Add your fixed task commands here
+if [ -n "$GITHUB_EVENT_PATH"];
+then
+    echo "GITHUB_EVENT_PATH is set to $GITHUB_EVENT_PATH"
+    echo "Contents of the event file:"
+    cat "$GITHUB_EVENT_PATH"
+    EVENT_PATH="$GITHUB_EVENT_PATH"
 else
-    echo "FIXED not found. Executing default task..."
-    # Add your default task commands here
+    echo "GITHUB_EVENT_PATH is not set. No JSON data to process."
+    exit 1
+fi
+
+env
+jq . < "$EVENT_PATH"
+
+
+if jq '.commits[].message, .head_commit.message' < "$EVENT_PATH" | grep -i -q "$*";
+then
+    echo "Keyword '$*' found in commit messages."
+else
+    echo "Keyword '$*' not found in commit messages."
 fi
 
 echo "Script execution completed."
